14b11a85a490fb2e16ee7e654effcb5554565170
Rodolphe <dutchr@live.fr>
2021-03-08 14:14:08 +0100

    adding create timer

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import EditableTimer from "./components/EditableTimer";
+import ToggleableTimeForm from "./components/ToggleableTimeForm";
+import uuidv4 from 'uuid/v4';
+import { newTimer } from "./utils/TimerUtils"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+state = {
+  timers : [
+    {
+      title :" tondre le gazon",
+      project: "Le jardin",
+      id: uuidv4(),
+      // elapsed : 5456099,
+      isRunning : true
+    },
+    {
+      title :" Faire à manger",
+      project: "Cuisine",
+      id: uuidv4(),
+      // elapsed : 1273998,
+      isRunning : false
+    },
+  ]
+}
+handleCreateFormSubmit = timer => {
+  const { timers } = this.state;
+  this.setState({ 
+    timers: [newTimer(timer), ...timers],
+  })
 }
 
+  render() {
+    const { timers} = this.state;
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimeForm onFormSubmit={this.handleCreateFormSubmit} />
+          {timers.map(( {title,project,id, elapsed, isRunning})=> (
+            <EditableTimer key={id}
+            id={id}
+            title={title}
+            project={project}
+            elapsed={elapsed}
+            isRunning={isRunning}
+            />
+          ))}
+          
+        </ScrollView>
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#D6D7DA",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  timerList: {
+    paddingBottom: 15,
   },
 });
